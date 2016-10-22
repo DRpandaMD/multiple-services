edd216740eb111efcce7fe93b7b51e9ca54105a4
Michael Zarate (Student) <mczarate@sundevils.asu.edu>
2016-10-22 12:54:43 -0700

    Trying to append to XML file null reference exception thrown

--- multiple-services/StoreCheckInService/XMLDataWriter.cs
+++ multiple-services/StoreCheckInService/XMLDataWriter.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Xml;
+using System.Xml.Linq;
 using System.Xml.Serialization;
 
 namespace StoreCheckInService
@@ -12,22 +14,38 @@ namespace StoreCheckInService
         
         public void writeToXMLFile(Store store)
         {
-            using (XmlWriter writer = XmlWriter.Create("stores.xml"))
+            string fileLocation = Path.Combine(HttpRuntime.AppDomainAppPath, @"App_Data");
+            fileLocation = Path.Combine(fileLocation, "stores.xml");
+
+
+            if (File.Exists(fileLocation))
             {
-                writer.WriteStartDocument();
-                writer.WriteStartElement("Stores");
-                writer.WriteStartElement("Store");
-                //writer.WriteElementString("ID", store._id.ToString());
-                writer.WriteElementString("StoreName", store.storeName);
-                writer.WriteElementString("StoreLocation", store.storeLocation);
-                writer.WriteElementString("HasCheckedIn", store.hasCheckedIn.ToString());
-                writer.WriteEndElement();
-                writer.WriteEndElement();
-                writer.WriteEndDocument();
+                XDocument doc = XDocument.Load(fileLocation);
+                XElement newStore = doc.Element("store");
+                newStore.Add(new XElement("StoreName", store.storeName),
+                             new XElement("StoreLocation", store.storeLocation),
+                             new XElement("HasCheckedIn", store.hasCheckedIn.ToString()));
+                doc.Save(fileLocation);
+                 
+            }
+            else
+            {
+                using (XmlWriter writer = XmlWriter.Create(fileLocation))
+                {
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("Stores");
+                    writer.WriteStartElement("Store");
+                    //writer.WriteElementString("ID", store._id.ToString());
+                    writer.WriteElementString("StoreName", store.storeName);
+                    writer.WriteElementString("StoreLocation", store.storeLocation);
+                    writer.WriteElementString("HasCheckedIn", store.hasCheckedIn.ToString());
+                    writer.WriteEndElement();
+                    writer.WriteEndElement();
+                    writer.WriteEndDocument();
 
-                //What happens if I have to append to a document?
-
-
+                    //What happens if I have to append to a document?
+                    
+                }
             }
         }
 
